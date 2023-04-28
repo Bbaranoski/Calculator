98897322c7163e0dad0571d2d47dd2b7b8c7fee4
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-04-28 16:08:22 -0300

    finish basic calculator

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
+firstNum = 1;
 
+secondNum = 1;
+
+operator = '+';
 
 function add(a, b) {
 
@@ -21,5 +25,33 @@ function multiply(a, b){
 function divide(a, b){
 
     return a / b;
+
+}
+
+function operate(firstNum, secondNum, operator){
+
+    switch(operator){
+
+        case '+':
+
+            return add(firstNum, secondNum);
+            break;
+
+        case '-':
+
+            return subtract(firstNum, secondNum);
+            break;
+
+        case '*':
+
+            return multiply(firstNum, secondNum);
+            break;
+
+        case '/':
+
+            return divide(firstNum, secondNum);
+            break;
+        
+    }
 
 }
